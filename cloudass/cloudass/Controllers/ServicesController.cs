2c906c152a8110773d7b68a445e9138e1733ba85
eason <ngyuxuan2003@gmail.com>
2025-04-17 16:43:36 +0800

    merge heng branch

---
+++ cloudass/cloudass/Controllers/ServicesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cloudass.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Teeth_cleaning()
+        {
+            return View();
+        }
+
+        public IActionResult Teeth_Bracing()
+        {
+            return View();
+        }
+
+        public IActionResult Tooth_Fillings()
+        {
+            return View();
+        }
+
+        public IActionResult Tooth_Extraction()
+        {
+            return View();
+        }
+    }
+}
